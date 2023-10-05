1d508ffb0632bde793df285a5d2cd4a35e492059
Rob Pear <tuanst130222@gmail.com>
2023-10-05 11:24:06 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
 const contactsRouter = require("./app/routes/contact.route");
-const ApiError = require("./app/api-error");
+const ApiError = require("./app/api-error").default;
+const app = express();
 
 app.use(cors());
 app.use(express.json());
@@ -10,8 +10,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the contact book application." });
 });
+
 app.use("/api/contacts", contactsRouter);
-module.exports = app;
 
 // Xử lý 404
 app.use((req, res, next) => {
@@ -20,5 +20,7 @@ app.use((req, res, next) => {
 
 // Xử lý lỗi chung
 app.use((err, req, res, next) => {
-  return res.status(err.statusCode || 500).json({ message: err.message || "Internal Server Error" });
+  return res.status(err.statusCode || 500).json({ message: err.message || "Internal Server Error"});
 });
+
+module.exports = app;
